491ef0eb16630c35941c910f2bcab0aaec39b05d
Eric Coutu <ecweb137@gmail.com>
2022-12-11 21:54:48 -0500

    added content in first and second file and added content file

--- firstfile.js
+++ firstfile.js
@@ -1,5 +1,9 @@
 /**THere is a tone of code in this file */
 
 function DOSTUFF(){
-    console.log('stuff is done here');
+    console.log('Changes made to stuff being done');
 }
+
+/**Made more changes on day 2 */
+
+/**even more changes */
